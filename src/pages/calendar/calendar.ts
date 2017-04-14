aac2a269dfc2422f3eee0222a31fe49a85400ea2
Daniel Fernandez Rico <deferrico@gmail.com>
2017-04-14 22:50:35 +0200

    Added event parsing as list on calendar, added listener for date to parse events

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,10 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Http} from '@angular/http';
+
 @Component({
     selector: 'calendar',
     templateUrl: './calendar.html'
 })
 export class Calendar implements OnInit {
-    constructor() { }
-    ngOnInit() { }
+
+	data: Array<{name: string, date: string}>;
+	events: Array<{name: string, date: string}>;
+
+    constructor(private http: Http) {
+    	this.http.get("./assets/events.json").subscribe(data => {
+        	this.data = JSON.parse(data['_body']);
+        	this.getData(14)
+        // console.log(this.items);
+    	});
+    }
+
+    getData(date) {
+    	this.events = this.data.filter(d => d.date == date);
+    }
+
+    translateDate(ev){
+    	// console.log(ev.detail.date);
+    	this.getData(ev.detail.date.getDate());
+    }
+
+    ngOnInit() { 
+	    document
+	    	.querySelector('.calendar')
+			.addEventListener('date-change', (e) => {
+				this.translateDate(e);
+			});
+
+    }
 }
