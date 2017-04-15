6a0455d45e4eabd4a1f6fb710ae03a60860c8413
Daniel Fernandez Rico <deferrico@gmail.com>
2017-04-15 10:55:46 +0200

    Added more events, fixed list display scroll, added human-readable date for list title

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -11,20 +11,37 @@ export class Calendar implements OnInit {
 	data: Array<{name: string, date: string}>;
 	events: Array<{name: string, date: string}>;
 	free: boolean;
+	date: string;
+	today: number;
 
     constructor(private http: Http) {
     	this.http.get("./assets/events.json").subscribe(data => {
         	this.data = JSON.parse(data['_body']);
-        	this.getData(14)
         // console.log(this.items);
     	});
     	this.free = false;
+    	this.today = new Date().getDate();
     }
 
-    getData(date) {
-    	this.events = this.data.filter(d => d.date == date);
+    getData(date: number) {
+    	this.events = this.data.filter(d => parseInt(d.date) == date);
     	if(this.events.length==0) this.free = true;
     	else this.free = false;
+
+    	switch (date) {
+    		case this.today:
+    			this.date = "hoy";
+    			break;
+    		case this.today-1:
+    			this.date = "ayer";
+    			break;
+    		case this.today+1:
+    			this.date = "mañana";
+    			break;
+    		default:
+    			this.date = "El día "+date;
+    			break;
+    	}
     }
 
     translateDate(ev){
