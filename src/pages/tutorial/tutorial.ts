94f796632208383d9d032473245dc7b5fd238b42
Daniel Fernandez Rico <deferrico@gmail.com>
2017-04-29 23:09:19 +0200

    Fixed selector for date in tutorial view

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -10,13 +10,32 @@ import { NavController } from 'ionic-angular';
 export class Tutorial {
 
   profesores: string = "n64";
+  tabla = [{
+    "dia": "Lunes",
+    "horario": "13-15"
+  },{
+    "dia": "Martes",
+    "horario": "10-11"
+  }];
+
+  today = new Date();
+  minutes = this.today.getMinutes().toString().length==1 ? 0+this.today.getMinutes().toString() : this.today.getMinutes().toString();
+  day = this.today.getDay().toString().length == 1 ? 0+this.today.getDay().toString() : this.today.getDay().toString;
+  months = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+  tutoria = {
+    "hora": this.today.getHours()+':'+this.minutes,
+    // "dia": this.day+' de '+this.months[this.today.getMonth()]
+    "dia": this.today.toISOString()
+  }
+
 
   constructor(public navCtrl: NavController
   ) {
-
+     console.log(this.tutoria);
   }
 
   debug(){
     console.log('Fn call working correctly')
+    console.log(this.tutoria);
   }
 }
